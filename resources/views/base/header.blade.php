a4ed79597f9df0d287316642f020ab456bb89706
Jimmy Lai <jimmy050311@gmail.com>
2023-11-12 14:29:21 +0800

    meta

--- resources/views/base/header.blade.php
+++ resources/views/base/header.blade.php
@@ -1,11 +1,28 @@
 <head>
-    <title>Stradale</title>
-    <link rel="icon" href="images/icon.png" type="image/gif" sizes="16x16">
-    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-    <meta content="" name="description" />
-    <meta content="" name="keywords" />
-    <meta content="" name="author" />
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta property="og:title" content="不老松" />
+    <meta property="og:image" content="/images/picture/icon_young.jpg" />
+    <meta property="og:description" content="不老松官方網站" />
+	<link rel="shortcut icon" href="/images/picture/icon_young.jpg" type="image/x-icon">
+	<link rel="icon" href="/images/picture/icon_young.jpg" type="image/x-icon">
+	<link href="/css/bootstrap.css" rel="stylesheet">
+	<link href="/css/style.css" rel="stylesheet">
+	<link href="/css/responsive.css" rel="stylesheet">
+	<link href="/css/responsive-2.css" rel="stylesheet">
+	<link href="/css/base.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Jost:wght@100;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
+	<title>不老松</title>
+    
+
+	<script src='https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.12/vue.js'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/qs/6.9.3/qs.js'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/axios/0.20.0/axios.js'></script>
+
+	<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <!-- CSS Files
     ================================================== -->
     <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" id="bootstrap" />
