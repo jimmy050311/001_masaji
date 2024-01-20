8e5d884aa249e675d96d19d1d5ee17d10ad53977
Jimmy Lai <jimmy050311@gmail.com>
2024-01-21 00:54:19 +0800

    view

--- resources/views/base/header.blade.php
+++ resources/views/base/header.blade.php
@@ -2,15 +2,15 @@
     <meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-    <meta property="og:title" content="尋求眼睛刺激" />
+    <meta property="og:title" content="-" />
     <meta property="og:image" content="/images/picture/icon_young.jpg" />
-    <meta property="og:description" content="尋求眼睛刺激" />
+    <meta property="og:description" content="-" />
 	<link rel="shortcut icon" href="/images/picture/icon_young.jpg" type="image/x-icon">
 	<link rel="icon" href="/images/picture/icon_young.jpg" type="image/x-icon">
 	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Jost:wght@100;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
-	<title>尋求眼睛刺激</title>
+	<title>-</title>
     
 
 	<script src='https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.12/vue.js'></script>
